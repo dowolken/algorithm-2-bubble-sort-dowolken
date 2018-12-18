f945fbea9d3fc3760641a961f3a0be0e0f499c02
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-18 09:36:31 +0800

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,1 +1,12 @@
 
+int[] a={5,1,2,4,3};15243 12543 12453 12435
+int large=0;
+int b=a.length;
+while(b!==0)
+     {for(int i=0;i<a.length-1;i++)
+        {if(a[i]>a[i+1])
+           {large=a[i];
+            a[i]=a[i+1];
+            a[i+1]=top;}
+        }
+     b--;} 
